e7b4d96a2a5dd56c21e7bb7bd6640eb8d4b61578
Nischal Aryal <37008195+Nischal5123@users.noreply.github.com>
2023-05-23 19:27:56 -0700

    Up-to-date-code
    -environment with 3 actions
    -Actor critic with hyperparameter file setup

--- ForeCache_Models/TDLearning.py
+++ ForeCache_Models/TDLearning.py
@@ -1,19 +1,18 @@
-import pdb
-
+import misc
 import numpy as np
 from collections import defaultdict
-import pandas as pd
 import itertools
-import matplotlib.pyplot as plt
-import sys
-import plotting
-import environment2
-from tqdm import tqdm
+import environment2 as environment2
+import multiprocessing
+import time
+import random
 
-class TDlearning:
+
+class TDLearning:
     def __init__(self):
         pass
 
+    # @jit(target ="cuda")
     def epsilon_greedy_policy(self, Q, epsilon, nA):
         """
         Creates an epsilon-greedy policy based on a given Q-function and epsilon.
@@ -29,15 +28,20 @@ class TDlearning:
         """
 
         def policy_fnc(state):
-            A = np.ones(nA, dtype=float) * epsilon / nA
-            best_action = np.argmax(Q[state])
-            A[best_action] += (1.0 - epsilon)
-
-            return A
+            coin = random.random()
+            if coin < epsilon:
+                if state == "Navigation":
+                    best_action = random.randint(0, 2)
+                else:
+                    best_action = random.randint(0, 1)
+            else:
+                best_action = np.argmax(Q[state])
+            return best_action
 
         return policy_fnc
 
 
+    # @jit(target ="cuda")
     def q_learning(self, user, env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.5):
         """
         Q-Learning algorithm: Off-policy TD control. Finds the optimal greedy policy
@@ -55,131 +59,91 @@ class TDlearning:
             Q is the optimal action-value function, a dictionary mapping state -> action values.
             stats is an EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
         """
+        Q = {
+            'Navigation':  np.zeros(3),
+            'Foraging': np.zeros(2),
+            'Sensemaking': np.zeros(2)
+        }
 
-        # The final action-value function.
-        # A nested dictionary that maps state -> (action -> action-value).
-        Q = defaultdict(lambda: np.zeros(len(env.valid_actions)))
-
-        # Keeps track of useful statistics
-        stats = plotting.EpisodeStats(
-            episode_lengths=np.zeros(num_episodes),
-            episode_rewards=np.zeros(num_episodes))
-
-        # The policy we're following
-        policy = self.epsilon_greedy_policy(Q, epsilon, len(env.valid_actions))
-
-        for i_episode in tqdm(range(num_episodes)):
-            # Print out which episode we're on, useful for debugging.
-            if (i_episode + 1) % 100 == 0:
-                print("\rEpisode {}/{}.".format(i_episode + 1, num_episodes), end="")
-                sys.stdout.flush()
+        for i_episode in range(num_episodes):
+            # The policy we're following
+            policy = self.epsilon_greedy_policy(Q, epsilon, len(env.valid_actions))
 
             # Reset the environment and pick the first state
             state = env.reset()
-
-            # One step in the environment
-            # total_reward = 0.0
-            # print("episode")
+            training_accuracy=[]
             for t in itertools.count():
                 # Take a step
-                action_probs = policy(state)
-                action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
-                next_state, reward, done, _ = env.step(state, action, False)
-                # pdb.set_trace()
-                # Update statistics
-                stats.episode_rewards[i_episode] += reward
-                stats.episode_lengths[i_episode] = t
+                action = policy(state)
+
+                next_state, reward, done, info,_ = env.step(state, action, False)
+
+                training_accuracy.append(info)
 
                 # TD Update
                 best_next_action = np.argmax(Q[next_state])
                 td_target = reward + discount_factor * Q[next_state][best_next_action]
                 td_delta = td_target - Q[state][action]
-                Q[state][action] += alpha * td_delta
-                # pdb.set_trace()
+                Q[state][action] += alpha * (td_delta + info)
+                state = next_state
                 if done:
                     break
-                state = next_state
+
         # print(policy)
-        return Q, stats
+        return Q, np.mean(training_accuracy)
 
-    def test(self, env, Q, epsilon=1):
 
-        policy = self.epsilon_greedy_policy(Q, epsilon, len(env.valid_actions))
-        discount_factor = 1.0
-        alpha = 0.5
-        # Reset the environment and pick the first action
-        state = env.reset(all = False, test=True)
-        valid_actions = ["same", "change"]
-        stats = []
-        # One step in the environment
-        # total_reward = 0.0
-        for t in itertools.count():
-            # Take a step
-            action_probs = policy(state)
-            action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
-            next_state, reward, done, prediction = env.step(state, action, True)
+    def test(self, env, Q, discount_factor, alpha, epsilon, num_episodes=1):
+        epsilon = epsilon
 
-            stats.append(prediction)
-            # print(prediction)
-            best_next_action = np.argmax(Q[next_state])
-            print(valid_actions[best_next_action])
-            td_target = reward + discount_factor * Q[next_state][best_next_action]
-            td_delta = td_target - Q[state][action]
-            Q[state][action] += alpha * td_delta
+        for i_episode in range(1):
 
-            if done:
-                break
+            state = env.reset(all=False, test=True)
+            stats = []
+            split_accuracy=defaultdict(list)
 
-            state = next_state
+            model_actions = []
+            reward_accumulated=[0.000000000000000000001]
+            reward_possible=[0.000000000000000000001]
+            policy = self.epsilon_greedy_policy(Q, epsilon, len(env.valid_actions))
 
-        cnt = 0
-        for i in stats:
-            cnt += i
-        cnt /= len(stats)
-        print("Accuracy of State Prediction: {}".format(cnt))
-        # plt.imshow(Q, interpolation='none')
-        # plt.show()
-        return cnt
+            for t in itertools.count():
+                # Take a step
+
+                action = policy(state)
+                model_actions.append(action)
+                next_state, reward, done, prediction,true_reward = env.step(state, action, True)
+                reward_accumulated.append(reward)
+
+                split_accuracy[state].append(prediction)
+                stats.append(prediction)
+                reward_accumulated.append(reward)
+                reward_possible.append(true_reward)
+
+                # print(prediction)
+                # Turning off the Q-Learning update when testing, the prediction is based on the Learned model from first x% interactions
+                best_next_action = np.argmax(Q[next_state])
+                td_target = reward + discount_factor * Q[next_state][best_next_action]
+                td_delta = td_target - Q[state][action]
+                Q[state][action] += alpha * (td_delta + prediction)
+
+                state = next_state
+                if done:
+                    break
+
+        return np.mean(stats),model_actions,split_accuracy, np.mean(reward_accumulated)/np.mean(reward_possible)
+
 
 if __name__ == "__main__":
-    all_accuracies=[]
-    all_testruns=[]
-    for testruns in range(100):
-        accuracies = []
-        env = environment2.environment2()
-        all_testruns.append(testruns)
-        users = env.user_list
-
-        thres = 0.8 #the percent of interactions Q-Learning will be trained on
-        for u in users:
-            print('########For user#############',u)
-            env.process_data(u, thres)
-            obj = TDlearning()
-            Q, stats = obj.q_learning(u, env, 500)
-            # plotting.plot_episode_stats(stats)
-            # env.take_step_subtask()
-            # print(Q)
-            accuracies.append(obj.test(env, Q))
-            # print("OK")
-            env.reset(True, False)
-
-    plt.plot(all_testruns,accuracies)
-    plt.show()
-
-
-#learn a policy a user will follow, convergance policy -> is too strict? Too much fluctuation
-# MDP might not reflect -> how user's learn
-#3rd possiblity -> humans follow different learning models in different setting.
-# When humans face a problem with lot of actions -> model-free algorithms
-# when environment is not complex -> model-based
-
-# Completely open-ended workload
-# Making sure uniform task design, correct design, relation between action and states.
-# Criteria of convergence should not be too strict
-
-# Difference between offline and online RL (Both, Relationship, with our work)
-# ideally we would like to simulate what happened in the simulation when the user's participated in the system
-# What we should do in response?
-#
+    start_time = time.time()
+    env = environment2.environment2()
+    user_list_2D = env.user_list_2D
+    obj2 = misc.misc(len(user_list_2D))
+    p8 = multiprocessing.Process(target=obj2.hyper_param, args=(env,user_list_2D, 'QLearn',50,))
+    p10 = multiprocessing.Process(target=obj2.hyper_param, args=(env, user_list_2D, 'SARSA',50,))
+    p8.start()
+    p10.start()
+    p8.join()
+    p10.join()
 
 
--- ForeCache_Models/environment2.py
+++ ForeCache_Models/environment2.py
@@ -4,27 +4,47 @@ import pdb
 from collections import defaultdict
 import glob
 import pandas as pd
+import numpy as np
+import random
+from collections import deque
+
 
 class environment2:
     def __init__(self):
-        self.user_list = glob.glob('taskname_ndsi-2d-task_*')
+        """
+        Constructor for the environment class.
+
+        Initializes required variables and stores data in memory.
+        """
+        self.user_list_2D = np.sort(glob.glob("data/NDSI-2D/U_*"))
+        self.user_list_3D = np.sort(glob.glob("data/NDSI-3D/taskname_ndsi-3d-task_*"))
+
         # This variable will be used to track the current position of the user agent.
         self.steps = 0
         self.done = False  # Done exploring the current subtask
-        self.valid_actions = ["same", "change"]
+
+        # List of valid actions and states
+        self.valid_actions = ["change", "same", "changeout"]
+        self.valid_states = ["Foraging", "Navigation", "Sensemaking"]
+
         # Storing the data into main memory. Focus is now only on action and states for a fixed user's particular subtask
         self.mem_states = []
         self.mem_reward = []
         self.mem_action = []
+        self.mem_roi = []
         self.threshold = 0
         self.prev_state = None
 
-    def reset(self, all=False, test = False):
-        # Resetting the variables used for tracking position of the agents
+    def reset(self, all=False, test=False):
+        """
+        Reset the variables used for tracking position of the agents.
+
+        :param all: If true, reset all variables.
+        :param test: If true, set the current step to threshold value.
+        :return: Current state.
+        """
         if test:
             self.steps = self.threshold
-            print("start {}".format(self.steps))
-            # pdb.set_trace()
         else:
             self.steps = 0
         self.done = False
@@ -32,50 +52,111 @@ class environment2:
             self.mem_reward = []
             self.mem_states = []
             self.mem_action = []
+            self.mem_roi = []
             return
 
         s, r, a = self.cur_inter(self.steps)
         return s
 
     def get_state(self, state):
+        """
+        Map 'Answering' state to 'Sensemaking' state.
+
+        :param state: Input state.
+        :return: Mapped state.
+        """
+        if state == "Answering":
+            state = "Sensemaking"
         return state
 
-    # Optimization is not the priority right now
-    # Returns all interactions for a specific user and subtask i.e. user 'P1', subtask '1.txt'
     def process_data(self, filename, thres):
-        # df = pd.read_excel(filename, sheet_name= "Sheet1", usecols="B:D")
+        """
+        Processes the data from the given file and stores it in the object memory.
+
+        Inputs:
+        - filename (str): path of the data file.
+        - thres (float): threshold value to determine the end of the episode.
+
+        Returns: None.
+        """
         df = pd.read_csv(filename)
-        self.prev_state = None
+
+        self.prev_state = "Foraging"
         cnt_inter = 0
+        roi_subset = []
+        subset = 1
         for index, row in df.iterrows():
-            # pdb.set_trace()
-            # print("here {} end\n".format(cnt_inter))
-            cur_state = self.get_state(row['State'])
-            if cur_state not in ('Foraging','Navigation', 'Sensemaking'):
+            cur_state = self.get_state(row["State"])
+            if cur_state not in ("Foraging", "Navigation", "Sensemaking"):
                 continue
             if self.prev_state == cur_state:
                 action = "same"
             else:
-                action = "change"
+                if self.prev_state == "Navigation":
+                    if cur_state == "Sensemaking":
+                        action = "change"
+                    else:
+                        action = "changeout"
+                else:
+                    action = "change"
+
+            if cur_state == "Sensemaking":
+                if (index < (len(df) - 1)) and df["State"][index + 1] != "Sensemaking":
+                    roi_subset.append(subset)
+                    subset = subset + 1
+                else:
+                    roi_subset.append(subset)
+            else:
+                roi_subset.append(subset)
+
             self.mem_states.append(cur_state)
-            self.mem_reward.append(row['NDSI'])
+            self.mem_reward.append(row["NDSI"] * row["ZoomLevel"])
             self.mem_action.append(action)
             cnt_inter += 1
-            self.prev_state=cur_state
+            self.prev_state = cur_state
+
+        self.mem_action = self.mem_action[1:] + ["same"]
+        self.mem_roi = roi_subset
         self.threshold = int(cnt_inter * thres)
-        print("{} {}\n".format(len(self.mem_states), self.threshold))
 
     def cur_inter(self, steps):
+        """
+        Returns the current state, reward, and action from the object memory.
+
+        Inputs:
+        - steps (int): current step of the episode.
+
+        Returns:
+        - (str): current state.
+        - (float): current reward.
+        - (str): current action.
+        """
         return self.mem_states[steps], self.mem_reward[steps], self.mem_action[steps]
 
     def peek_next_step(self):
+        """
+        Returns whether the next step exists and its index.
+
+        Inputs: None.
+
+        Returns:
+        - (bool): True if the next step does not exist, False otherwise.
+        - (int): index of the next step.
+        """
         if len(self.mem_states) > self.steps + 1:
             return False, self.steps + 1
         else:
             return True, 0
 
-    def take_step_action(self, test = False):
-        # pdb.set_trace()
+    def take_step_action(self, test=False):
+        """
+        Takes a step in the episode.
+
+        Inputs:
+        - test (bool): whether to take a test step.
+
+        Returns: None.
+        """
         if test:
             if len(self.mem_states) > self.steps + 1:
                 self.steps += 1
@@ -89,26 +170,70 @@ class environment2:
                 self.done = True
                 self.steps = 0
 
-    # act_arg = action argument refers to action number
+    def get_transition(self, current_state, action_index):
+        """
+        Given the current state and the index of the chosen action, returns the next state.
+
+        Args:
+        - current_state (str): the current state of the environment.
+        - action_index (int): the index of the chosen action in the list of valid actions.
+
+        Returns:
+        - (str): the next state of the environment.
+        """
+        if current_state == "Navigation":
+            if action_index == 0:
+                next_state = "Navigation"
+            elif action_index == 1:
+                next_state = "Sensemaking"
+            else:
+                next_state = "Foraging"
+        elif current_state == "Foraging":
+            if action_index == 1:
+                next_state = "Navigation"
+            else:
+                next_state = "Foraging"
+        else:
+            if action_index == 1:
+                next_state = "Navigation"
+            else:
+                next_state = "Sensemaking"
+        return next_state
+
     def step(self, cur_state, act_arg, test):
+        # Get the current state, reward, and action at the current step
         _, cur_reward, cur_action = self.cur_inter(self.steps)
+
+        # Peek at the next step to get the next state, reward, and action
         _, temp_step = self.peek_next_step()
         next_state, next_reward, next_action = self.cur_inter(temp_step)
-        # pdb.set_trace()
-        predicted_action=self.valid_actions[act_arg]
+
+        # Get the predicted next state based on the current state and the action taken
+        # model_next_state = self.get_transition(cur_state, act_arg)
+
+        # Get the predicted action based on the action argument
+        predicted_action = self.valid_actions[act_arg]
+
+        # Initialize top_reward to 0
+        top_reward = 0
+
+        # If the predicted action is the same as the current action,
         if predicted_action == cur_action:
             prediction = 1
         else:
+            # Otherwise, set prediction to 0 and cur_reward to 0
             prediction = 0
+            cur_reward = 0
+
+        # Set top_reward to cur_reward
+        top_reward = cur_reward
+
+        # Take the step action
         self.take_step_action(test)
-        return next_state, cur_reward, self.done, prediction
+
+        # Return the predicted next state, current reward, done status, prediction, and top reward
+        return next_state, cur_reward, self.done, prediction, top_reward
 
 
 if __name__ == "__main__":
     env = environment2()
-    users = env.user_list
-    print(users)
-    # env.get_subtasks(users[0])
-    # for idx in range(len(env.mem_states)):
-    #     print("{} {}".format(env.mem_states[idx], env.mem_reward[idx]))
-    # print(users)
