aac536932829071eb5e451f8b7a735ccc14217a3
Nischal Aryal <37008195+Nischal5123@users.noreply.github.com>
2024-09-22 20:16:55 -0400

    Reinforce EXP

--- ForeCache_Models/Reinforce-One-Model.py
+++ ForeCache_Models/Reinforce-One-Model.py
@@ -71,14 +71,17 @@ class Policy(nn.Module):
 
 
 class Reinforce():
-    def __init__(self,env,learning_rate,gamma,tau,dataset):
+    def __init__(self,env,learning_rate,gamma,tau,dataset, learned_policy=None):
         self.env = env
         if dataset == 'birdstrikes':
             self.total_length = 14
         else:
             self.total_length = 16
         self.learning_rate, self.gamma, self.temperature = learning_rate, gamma, tau
-        self.pi = Policy(self.learning_rate, self.gamma,self.temperature,dataset)
+        if learned_policy is None:
+            self.pi = Policy(learning_rate, gamma, tau, dataset)
+        else:
+            self.pi = learned_policy
 
     def convert_idx_state(self, state_idx):
         #state = next((key for key, value in self.state_encoding.items() if np.array_equal(value, state_idx)), None)
@@ -142,15 +145,19 @@ class Reinforce():
             s = np.array(self.convert_state_idx(s))
             done = False
             predictions = []
-            actions = []
+            predicted_action = []
+            all_ground_actions = []
+            insight = defaultdict(list)
 
             while not done:
                 prob = policy(torch.from_numpy(s).float())
                 m = Categorical(prob)
                 a = m.sample().item()
-                actions.append(a)
-                s_prime, r, done, info,true_reward = self.env.step(self.convert_idx_state(s), a, True)
+                s_prime, r, done, info,_,ground_action, pred_action,_ = self.env.step(self.convert_idx_state(s), a, True)
                 predictions.append(info)
+                predicted_action.append(pred_action)
+                all_ground_actions.append(ground_action)
+                insight[ground_action].append(info)
                 #split_accuracy[self.convert_idx_state(s)].append(info)
 
 
@@ -166,7 +173,11 @@ class Reinforce():
             #print("TEST: # of episode :{}, accuracy : {}, actions: {}".format(n_epi, np.mean(predictions), Counter(actions)))
 
             test_accuracies.append(np.mean(predictions))
-        return np.mean(test_accuracies),split_accuracy,0
+            granular_prediction = defaultdict()
+            for keys, values in insight.items():
+                granular_prediction[keys] = (len(values), np.mean(values))
+
+        return np.mean(test_accuracies),granular_prediction, predicted_action, all_ground_actions
 
 def training(train_files, env, dataset, algorithm, epoch):
     # Load hyperparameters from JSON file
@@ -183,11 +194,10 @@ def training(train_files, env, dataset, algorithm, epoch):
         for ga in gammas:
             accu = []
             model = None
-
             for user in train_files:
-                env.reset(True)
+                env= environment5.environment_vizrec()
                 env.process_data(user, 0)
-                agent = Reinforce(env, lr, ga, 1, dataset)
+                agent = Reinforce(env, lr, ga, 1, dataset, model) #model is the learned policy from previous users
 
                 model, accu_user = agent.train()
                 accu.append(accu_user)
@@ -199,48 +209,51 @@ def training(train_files, env, dataset, algorithm, epoch):
                 best_gamma = ga
                 best_ac_model = model
 
-    return best_ac_model, best_lr, best_gamma, max_accu
+    return agent, best_ac_model, best_lr, best_gamma, max_accu
 
 
-def testing(test_files, env, trained_ac_model, best_lr, best_gamma, dataset, algorithm):
+def testing(agent, test_files, env, trained_ac_model, best_lr, best_gamma, dataset, algorithm):
     final_accu = []
     for user in test_files:
         env.reset(True)
         env.process_data(user, 0)
-        accu, granular_acc = agent.test(trained_ac_model)
+        accu, granular_acc, predicted_actions, ground_actions = agent.test(trained_ac_model)
         final_accu.append(accu)
 
-    return np.mean(final_accu), granular_acc
+    return np.mean(final_accu), granular_acc, predicted_actions, ground_actions
 
 
 def process_user(user_data):
     test_user_log, train_files, env, d, algorithm, epoch = user_data
 
     # Train the model
-    trained_ac_model, best_lr, best_gamma, training_accuracy = training(train_files, env, d, algorithm, epoch)
+    agent, trained_ac_model, best_lr, best_gamma, training_accuracy = training(train_files, env, d, algorithm, epoch)
 
     # Test the model
     test_files = [test_user_log]
     best_accu = -1
     best_granular_acc = {}
     for _ in range(5):
-        testing_accu, granular_acc = testing(test_files, env, trained_ac_model, best_lr, best_gamma, d, algorithm)
+        testing_accu, granular_acc, predicted_actions, ground_actions = testing(agent, test_files, env, trained_ac_model, best_lr, best_gamma, d, algorithm)
         if testing_accu > best_accu:
             best_accu = testing_accu
             best_granular_acc = granular_acc
+            best_predicted_actions = predicted_actions
+            best_ground_actions = ground_actions
 
-    return [test_user_log, best_accu, best_granular_acc]
+    return [test_user_log, best_accu, best_granular_acc, best_predicted_actions, best_ground_actions]
 
 
 if __name__ == "__main__":
     env = environment5.environment_vizrec()
-    datasets = ['movies']
-    tasks = ['p3', 'p4', 'p1', 'p2']
+    datasets = ['movies', 'birdstrikes']
+    tasks = ['p1', 'p2','p3', 'p4']
     overall_accuracy = []
 
     for d in datasets:
         for task in tasks:
             final_output = []
+            env = environment5.environment_vizrec()
 
             # Get the user list
             user_list = list(env.get_user_list(d, task))
@@ -256,18 +269,18 @@ if __name__ == "__main__":
             with concurrent.futures.ProcessPoolExecutor() as executor:
                 futures = [executor.submit(process_user, user_data) for user_data in user_data_list]
                 for future in concurrent.futures.as_completed(futures):
-                    test_user_log, best_accu, best_granular_acc = future.result()
+                    test_user_log, best_accu, best_granular_acc, best_predicted_actions, best_ground_actions = future.result()
                     accuracies.append(best_accu)
-                    final_output.append([test_user_log, best_accu, best_granular_acc])
+                    final_output.append([test_user_log, best_accu, best_granular_acc,str(best_predicted_actions), str(best_ground_actions)])
 
             # Save dataset and task level data
             final_output = np.array(final_output)
             directory = f"Experiments_Folder/VizRec/{d}/{task}"
             os.makedirs(directory, exist_ok=True)
-            np.savetxt(f"{directory}/AC-Single-Model.csv", final_output, delimiter=",", fmt='%s')
+            np.savetxt(f"{directory}/Reinforce-Single-Model.csv", final_output, delimiter=",", fmt='%s')
 
             test_accu = np.mean(accuracies)
-            print(f"Dataset: {d}, Task: {task}, ActorCritic, {test_accu}")
+            print(f"Dataset: {d}, Task: {task}, Reinforce, {test_accu}")
             overall_accuracy.append(test_accu)
 
     print(f"Overall Accuracy: {np.mean(overall_accuracy)}")
